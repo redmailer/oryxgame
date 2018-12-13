c3c5a88c43bddbb9092422600135c14828348b29
redmailer <xxjs010320@163.com>
2018-12-13 11:15:31 +0800

    fix

--- proto/makeproto.sh
+++ proto/makeproto.sh
@@ -13,8 +13,8 @@ for file in ./pbtxt/*.proto
 do
 	if test -f $file
         then
-                echo ./protocolbuf-linux/bin/protoc --cpp_out=./pb/ --proto_path=./pbtxt $file
-		./protocolbuf-linux/bin/protoc --cpp_out=./pb/ --proto_path=./pbtxt $file
+                echo ../thirdSource/bin/protoc --cpp_out=./pb/ --proto_path=./pbtxt $file
+		../thirdSource/bin/protoc --cpp_out=./pb/ --proto_path=./pbtxt $file
         fi
 
 done
